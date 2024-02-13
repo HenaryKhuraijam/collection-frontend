ddbed40c9e12a82fe90b20257f4be87761219a0b
HenaryKhuraijam <henarykhu123@gmail.com>
2024-02-14 00:21:30 +0530

    Add login page and basic sytle for login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
+import Login from './pages/Login.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Hello world!!</h1>
-    </>
+   
+      <Login />
+
   )
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+
+
+const Login = () => {
+    return (
+        <div className="loginForm-contaniner">
+            
+            <form className="login-form" action="submit">
+            <h1>Welcome to Bros Group</h1>
+            <p>High Alert !!! If enter wrong data to Login we will Destroy your system</p>
+                <input type="text" placeholder="Enter your UserID" />
+                <input type="text" placeholder="Enter your Password" />
+                <button>Submit</button>
+                <a href="#">forgot password</a>
+            </form>
+        </div>
+    )
+}
+
+export default Login

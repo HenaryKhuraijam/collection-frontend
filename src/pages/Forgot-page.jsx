6d2a6151437c18c43db94c2f94b594f70dec0376
HenaryKhuraijam <henarykhu123@gmail.com>
2024-02-16 00:52:37 +0530

    Add forgot page UI

--- src/pages/Forgot-page.jsx
+++ src/pages/Forgot-page.jsx
@@ -1,7 +1,13 @@
 const Forgot = () => {
-    <>
-    <h1 id="forgot">Forgot page hit</h1>
-    </>
+    <div className="forgotForm-contaniner">
+            
+            <form className="forgot-form" action="submit">
+            <h1>Choose your data wisely before typing</h1>
+                <input type="text" placeholder="Enter your UserID" />
+                <input type="text" placeholder="Enter your Registration Number" />
+                <button>Submit</button>
+            </form>
+        </div>
 }
 
 export default Forgot
